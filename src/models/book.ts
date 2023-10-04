b97b9c93de4bc42f52d02db1cc615e684156b22e
BrigitteLPB <lpb.brigitte@gmail.com>
2023-10-05 00:06:42 +0200

    add book schema

---
+++ src/models/book.ts
@@ -0,0 +1,21 @@
+import { Schema, model } from "mongoose"
+
+export class BookSchema extends Schema {
+	constructor(){
+		super({
+			userId: {type: String, unique: true, required: true},
+			title: {type: String},
+			author: {type: String},
+			imageUrl: {type: String},
+			year: {type: Number},
+			genre: {type: String},
+			ratings: {type: Array<{
+				userId: String,
+				grade: Number
+			}>},
+			averageRating: {type: Number}
+		})
+	}
+}
+
+export const BookModel = model("Book", new BookSchema())
