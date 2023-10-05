e4e098237b7ddcb7cfd37060da8c3ca700513816
BrigitteLPB <lpb.brigitte@gmail.com>
2023-10-06 00:26:22 +0200

    add api-manager

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,24 @@
 
-import express, { Express, Request, Response } from 'express';
-import { AddressInfo } from 'net';
-const cors = require('cors');
+import { Request, Response } from 'express';
+import { ApiManager, HTTPMethod } from 'utils/api_manager';
 
-const app: Express = express();
+const api_manager = new ApiManager(
+	{
+		origin: process.env.CORS_ORIGINS?.split(','),
+		optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
+	},
+	[
+		{
+			method: HTTPMethod.GET,
+			uri: "/*",
+			middelware: (req: Request, res: Response) => {
+				console.log(req.header("Origin"));
 
-const cors_config = {
-	origin: process.env.CORS_ORIGINS?.split(','),
-	optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-}
+				console.log(req.body);
+				res.end(JSON.stringify(req.body));
+			}
+		}
+	]
+)
 
-
-app.use(express.json());
-
-
-app.use('/*', cors(cors_config), (req: Request, res: Response) => {
-	console.log(req.header("Origin"));
-
-	console.log(req.body);
-	res.end(JSON.stringify(req.body));
-})
-
-
-const server = app.listen(process.env.API_PORT, () => {
-	const server_info = (server.address() as AddressInfo);
-
-	const host = server_info.address;
-	const port = server_info.port;
-
-	console.log("Example app listening at http://%s:%s", host, port)
-})
+api_manager.run(process.env.API_PORT || 4000);
